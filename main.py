59b7a5be9fcb508bc901c8456c9e2a5f26be6b32
mhmukry <mhmukry@gmail.com>
2024-12-13 15:30:01 -0500

    commit main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-import os
+import os 
 
 if __name__ == '__main__':
     file_path = 'resources.csv'
     if not os.path.exists(file_path):
         open(file_path, 'w').close()
 
-    resource_manager = ResourceManager(file_path)
-    ui = UI(resource_manager)
 
-    # Load existing data from file
-    resource_manager.load_data()
-
-    # Start UI
-    ui.run()
-
-    # Save data to file on exit
-    resource_manager.save_data()
